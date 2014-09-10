68a9affecdf5168e99bc84f712d3d8a4d3f997c8
hs3180 <yfzbhs@infore.com>
2014-09-10 11:46:28 +0800

    fix dependency problem

--- R/src-sqlserver.r
+++ R/src-sqlserver.r
@@ -1,11 +1,13 @@
 #' create "src_sqlserver" object
 #' @import dplyr
+#' @import RMSSQL
+#' @import DBI
 #' @export
 src_sqlserver <- function(dbname, host = NULL, user = "root", 
                       password = "", ...) {
-  if (!require("RSQLServer")) {
-    stop("RSQLServer package required to connect to SQL Server", call. = FALSE)
-  }
+#   if (!require("RSQLServer")) {
+#     stop("RSQLServer package required to connect to SQL Server", call. = FALSE)
+#   }
   
   drv <- SQLServer() 
   
